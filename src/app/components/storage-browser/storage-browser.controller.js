1f2845d7b9ad6327ac9031b0c22e6f634e505465
Dave Gramlich <callmehiphop@gmail.com>
2015-09-24 19:19:59 -0400

    enhanced storage browser

--- src/app/components/storage-browser/storage-browser.controller.js
+++ src/app/components/storage-browser/storage-browser.controller.js
@@ -1,3 +1,4 @@
+/* global document:true */
 (function() {
   'use strict';
 
@@ -5,16 +6,58 @@
     .module('gcloudConsole')
     .controller('StorageBrowserCtrl', StorageBrowserCtrl);
 
-  function StorageBrowserCtrl($stateParams, GApi) {
+  /** @ngInject */
+  function StorageBrowserCtrl($mdDialog, $stateParams, GApi) {
     var plugin = this;
 
-    GApi.load('storage', 'v1');
+    plugin.refresh = getBuckets;
+    plugin.createBucket = createBucket;
 
-    GApi.executeAuth('storage', 'buckets.list', {
-      project: $stateParams.projectId
-    }).then(function(buckets) {
-      plugin.buckets = buckets.items;
-    });
+    GApi.load('storage', 'v1');
+    getBuckets();
+
+    function getBuckets() {
+      GApi.executeAuth('storage', 'buckets.list', {
+        project: $stateParams.projectId
+      }).then(function(buckets) {
+        plugin.buckets = buckets.items;
+      });
+    }
+
+    function createBucket($event) {
+      $mdDialog.show({
+        parent: angular.element(document.body),
+        targetEvent: $event,
+        templateUrl: 'app/components/storage-browser/storage-browser-dialog.html',
+        controller: BucketCtrl,
+        controllerAs: 'bucket',
+        bindToController: true
+      });
+    }
+
+    function BucketCtrl() {
+      var bucket = this;
+
+      bucket.create = create;
+      bucket.cancel = closeDialog;
+      bucket.storageClass = false;
+      bucket.location = 'US';
+
+      function create() {
+        GApi.executeAuth('storage', 'buckets.insert', {
+          project: $stateParams.projectId,
+          name: bucket.name
+        })
+        .then(function(newBucket) {
+          plugin.buckets.push(newBucket);
+        })
+        .then(closeDialog);
+      }
+
+      function closeDialog() {
+        $mdDialog.hide();
+      }
+    }
   }
 
   if (typeof module !== 'undefined') {
