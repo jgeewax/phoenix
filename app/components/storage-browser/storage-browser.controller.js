b457bbe33048fc257aed1a53e17dd4ee9cd8dcd5
Dave Gramlich <callmehiphop@gmail.com>
2015-09-26 03:23:18 -0400

    Updating after push to master

--- app/components/storage-browser/storage-browser.controller.js
+++ app/components/storage-browser/storage-browser.controller.js
@@ -4,23 +4,68 @@
 
   var mod = angular
     .module('gcloudConsole')
+    .config(routes)
     .controller('StorageBrowserCtrl', StorageBrowserCtrl);
 
   /** @ngInject */
-  function StorageBrowserCtrl($mdDialog, $stateParams, GApi) {
-    var plugin = this;
+  function routes($stateProvider) {
+    $stateProvider
+      .state('storage-browser', {
+        url: '/storage-browser',
+        parent: 'project',
+        controller: 'StorageBrowserCtrl',
+        controllerAs: 'browser',
+        templateUrl: 'app/components/storage-browser/storage-browser.html',
+        resolve: { buckets: getBuckets }
+      });
+  }
 
-    plugin.refresh = getBuckets;
-    plugin.createBucket = createBucket;
-
+  /** @ngInject */
+  function getBuckets($stateParams, GApi) {
     GApi.load('storage', 'v1');
-    getBuckets();
 
-    function getBuckets() {
-      GApi.executeAuth('storage', 'buckets.list', {
+    return GApi.executeAuth('storage', 'buckets.list', {
+      project: $stateParams.projectId
+    });
+  }
+
+  /** @ngInject */
+  function StorageBrowserCtrl($q, $mdDialog, $stateParams, GApi, buckets) {
+    var browser = this;
+
+    // props
+    browser.buckets = buckets.items;
+    browser.selected = [];
+    browser.filter = null;
+    browser.filtering = false;
+
+    // ui methods
+    browser.isIdle = isIdle;
+    browser.isSelected = isSelected;
+    browser.getVerbiage = getVerbiage;
+
+    // crud methods - refactor into service..?
+    browser.refreshBuckets = refreshBuckets;
+    browser.createBucket = createBucket;
+    browser.deleteBuckets = deleteBuckets;
+
+    function isIdle() {
+      return !browser.filtering && !isSelected();
+    }
+
+    function isSelected() {
+      return browser.selected.length > 0;
+    }
+
+    function getVerbiage() {
+      return browser.selected.length === 1 ? 'item' : 'items';
+    }
+
+    function refreshBuckets() {
+      return GApi.executeAuth('storage', 'buckets.list', {
         project: $stateParams.projectId
       }).then(function(buckets) {
-        plugin.buckets = buckets.items;
+        browser.buckets = buckets.items;
       });
     }
 
@@ -29,34 +74,53 @@
         parent: angular.element(document.body),
         targetEvent: $event,
         templateUrl: 'app/components/storage-browser/storage-browser-dialog.html',
-        controller: BucketCtrl,
-        controllerAs: 'bucket',
+        controller: DialogCtrl,
+        controllerAs: 'dialog',
         bindToController: true
-      });
+      })
+      .finally(refreshBuckets);
     }
 
-    function BucketCtrl() {
-      var bucket = this;
-
-      bucket.create = create;
-      bucket.cancel = closeDialog;
-      bucket.storageClass = false;
-      bucket.location = 'US';
-
-      function create() {
-        GApi.executeAuth('storage', 'buckets.insert', {
+    function deleteBuckets() {
+      var requests = browser.selected.map(function(bucket) {
+        return GApi.executeAuth('storage', 'buckets.delete', {
           project: $stateParams.projectId,
-          name: bucket.name
-        })
-        .then(function(newBucket) {
-          plugin.buckets.push(newBucket);
-        })
-        .then(closeDialog);
+          bucket: bucket.name
+        });
+      });
+
+      return $q.all(requests)
+        .finally(refreshBuckets);
+    }
+  }
+
+  /** @ngInject */
+  function DialogCtrl($mdDialog, $stateParams, GApi) {
+    var dialog = this;
+
+    dialog.storageClass = false;
+    dialog.location = 'US';
+
+    dialog.createBucket = createBucket;
+    dialog.close = closeDialog;
+
+    function createBucket() {
+      var options = {
+        project: $stateParams.projectId,
+        name: dialog.name,
+        location: dialog.location
+      };
+
+      if (dialog.storageClass) {
+        options.storageClass = dialog.storageClass;
       }
 
-      function closeDialog() {
-        $mdDialog.hide();
-      }
+      return GApi.executeAuth('storage', 'buckets.insert', options)
+        .then(closeDialog);
+    }
+
+    function closeDialog() {
+      $mdDialog.hide();
     }
   }
 
